403c8a968b08cf9f49525d908f12e413dc6dd622
RustikDev <rustiknevazno@gmail.com>
2025-05-17 20:23:31 +0300

    Хвост роутов

---
+++ english_hex/src/pages/AddCategoriesPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="layout">Добавить категорию</div>
+</template>
--- english_hex/src/router/routes.js
+++ english_hex/src/router/routes.js
@@ -7,8 +7,30 @@ const routes = [
   },
   {
     path: "/",
-    name: "mainPage",
-    component: () => import("@/pages/MainPage/MainPage.vue"),
+    name: "home",
+    component: () => import("@/App.vue"),
+    children: [
+      {
+        path: "",
+        name: "mainPage",
+        component: () => import("@/pages/MainPage.vue"),
+      },
+      {
+        path: "addCategories",
+        name: "addCategories",
+        component: () => import("@/pages/AddCategoriesPage.vue"),
+      },
+      {
+        path: "raiting",
+        name: "raiting",
+        component: () => import("@/pages/RaitingPage.vue"),
+      },
+      {
+        path: "profile",
+        name: "profile",
+        component: () => import("@/pages/ProfilePage.vue"),
+      },
+    ],
     meta: { requiresAuth: false },
   },
 ];
