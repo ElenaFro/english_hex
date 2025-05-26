6d5d11a745ca45f3a0a3bc644ca34bef73eb53a9
RustikDev <rustiknevazno@gmail.com>
2025-05-27 00:08:13 +0300

    Остановка аудио после выхода, смещение попапа вверх для мобилки

--- english_hex/src/components/popups/HelloPopup.vue
+++ english_hex/src/components/popups/HelloPopup.vue
@@ -4,14 +4,15 @@
             <p class="popup__header d-mb-16">
                 {{ headerText }}
             </p>
-            <img src="@/assets/img/close_icon.svg" class="popup__close" @click="emit('close')" />
+            <img src="@/assets/img/close_icon.svg" class="popup__close" @click="stopAndClose" />
             <div>
                 {{ bodyText }}
             </div>
             <div class="full_width flex justify-between">
                 <div class="flex items-end">
-                    <button class="arrow_btn"> <img src="@/assets/img/arrow_icon.svg" alt="arrow"
-                            @click="emit('close')"></button>
+                    <button class="arrow_btn" @click="stopAndClose">
+                        <img src="@/assets/img/arrow_icon.svg" alt="arrow" />
+                    </button>
                 </div>
                 <div class="relative">
                     <img src="@/assets/Di_avatar/girl_with_flag.svg" alt="girl with flag" class="girl_with_flag">
@@ -21,19 +22,49 @@
         </section>
     </section>
 </template>
-<script setup>
-import soundMp3 from '@/assets/audio/helloFromDi.mp3'
 
-const emit = defineEmits(['close'])
-const headerText = 'Добро пожаловать!'
-const bodyText = 'Привет! Меня зовут Di, и я рада приветствовать тебя в мире изучения английских слов! Ты сделал важный шаг к своей мечте - свободному владению иностранным языком.'
+<script setup>
+import { ref } from 'vue';
+import soundMp3 from '@/assets/audio/helloFromDi.mp3';
+
+const emit = defineEmits(['close']);
+const headerText = 'Добро пожаловать!';
+const bodyText = 'Привет! Меня зовут Di, и я рада приветствовать тебя в мире изучения английских слов! Ты сделал важный шаг к своей мечте - свободному владению иностранным языком.';
+
+const lastPlayed = ref(0);
+const COOLDOWN_MS = 15 * 1000;
+
+const currentAudio = ref(null);
 
 const playSound = () => {
+    const currentTime = Date.now();
+    if (currentTime - lastPlayed.value < COOLDOWN_MS) return
+
+    if (currentAudio.value) {
+        currentAudio.value.pause();
+        currentAudio.value.currentTime = 0;
+    }
+
     const audio = new Audio(soundMp3);
+    currentAudio.value = audio;
     audio.play().then(() => {
+        lastPlayed.value = currentTime;
     }).catch((error) => {
         console.error('Ошибка воспроизведения звука:', error);
     });
+
+    audio.onended = () => {
+        currentAudio.value = null;
+    };
+};
+
+const stopAndClose = () => {
+    if (currentAudio.value) {
+        currentAudio.value.pause();
+        currentAudio.value.currentTime = 0;
+        currentAudio.value = null;
+    }
+    emit('close');
 };
 </script>
 
@@ -53,6 +84,7 @@ const playSound = () => {
 .popup {
     position: relative;
     z-index: 400;
+    margin-bottom: 180px;
 
     &__close {
         position: absolute;
