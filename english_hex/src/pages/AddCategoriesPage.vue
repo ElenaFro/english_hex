2c98d3e443edb40b1df46bea883e4578317cd093
Elena Frolova <frolova.est@gmail.com>
2025-06-02 14:30:46 +0700

    360-414px странице + и уведомлений

--- english_hex/src/pages/AddCategoriesPage.vue
+++ english_hex/src/pages/AddCategoriesPage.vue
@@ -1,10 +1,22 @@
 <template>
-    <div>
+    <div  class="add-page">
 		<AddCategories />
 	</div>
 </template>
 <script setup>
 import AddCategories from '@/components/AddCategories.vue'
 </script>
-<style scoped>
+<style scoped lang="scss">
+.add-page {
+    height: 100vh;
+    width: 100vw; 
+    max-width: 414px; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: linear-gradient(135deg, #e0e7ff, #a3bffa);
+    border-top-left-radius: 40px;
+    border-top-right-radius: 40px;
+
+}
 </style>
--- english_hex/src/pages/NotificationsPage.vue
+++ english_hex/src/pages/NotificationsPage.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
+    <div class="notification-page">
 		<Notification />
 	</div>
 </template>
 <script setup>
 import Notification from '@/components/Notification.vue'
 </script>
-<style scoped>
+<style scoped lang="scss">
+.notification-page {
+    height: 100vh;
+    width: 100vw; 
+    max-width: 414px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: linear-gradient(135deg, #e0e7ff, #a3bffa);
+    border-top-left-radius: 40px;
+    border-top-right-radius: 40px;
+}
 </style>
