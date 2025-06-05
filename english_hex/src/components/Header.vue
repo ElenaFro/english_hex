10c35dd8368972fcc02132cbfba9a13e10b89e50
Elena Frolova <frolova.est@gmail.com>
2025-06-05 12:26:46 +0700

    хедер: рейтинг и жизни для игры про планету

--- english_hex/src/components/Header.vue
+++ english_hex/src/components/Header.vue
@@ -5,6 +5,14 @@
                 {{ earnedStars }} <img src="@/assets/icons/navBarIcon/star.svg" class="header-star-left" alt="Звезда" />
             </span>
         </template> 
+
+        <template v-else-if="isGamePlanetPage"> <!-- Добавлено условие для страницы Game Planet -->
+            <span class="header-live">
+                <span v-for="star in liveStars" :key="star" class="live-icon">
+                    <img src="@/assets/icons/navBarIcon/live.svg" class="header-live-left" alt="Жизнь" />
+                </span>
+            </span>
+        </template>
         <template v-else>      
             <button @click="goBack" class="header-item-button">
                 <img src="@/assets/icons/navBarIcon/arrow_left.svg" class="header-icon-left" alt="Назад" />
@@ -15,6 +23,11 @@
             :class="{ active: $route.path === item.path }">
             <img v-if="!route.fullPath.includes('games') && !route.fullPath.includes('Game')" :src="item.icon" class="header-icon" :alt="item.name" />
         </RouterLink>
+        <template v-if="isGamePlanetPage">
+            <span class="header-star">
+                {{ earnedStars }} <img src="@/assets/icons/navBarIcon/star.svg" class="header-star-left" alt="Звезда" />
+            </span>
+        </template> 
     </div>
 
 </template>
@@ -24,16 +37,10 @@ import { RouterLink } from "vue-router";
 import { ref, watch, computed  } from 'vue';
 import { useRoute} from 'vue-router';
 
-// const headerItemsLeft = [
-//     {
-//         name: "left",
-//         path: "/",
-//         icon: 'src/assets/icons/navBarIcon/arrow_left.svg',
-//     },
-// ];
-
 const earnedStars = ref(0); // Здесь вы можете динамически изменять количество звезд
+const liveStars = ref(5);
 const isHomePage = computed(() => route.path === '/'); // Проверяем, находимся ли мы на главной странице
+const isGamePlanetPage = computed(() => route.path === '/planetAttackGame'); // Проверяем, находимся ли мы на главной странице
 
 const headerItemsRight = [
     {
@@ -117,11 +124,29 @@ watch(() => route.path, (newPath) => {
     padding-bottom: 0px;
     align-items: center;
 }
+.header-live {
+    background-color: #ffffff;
+    display: flex;
+    width: 155px;
+    height: 42px;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    padding-left: 8px;
+    padding-right: 3px;
+    padding-bottom: 0px;
+    align-items: center; 
+    gap: 5px; 
+}
 .header-star-left {
     width: 21px;
     height: 21px;
     padding-left: 2px;
     padding-bottom: 2px;
+}
+
+.header-live-left {
+    width: 23.85px;
+    height: 22.19px;
 }
 
 .header-item {
@@ -171,5 +196,9 @@ watch(() => route.path, (newPath) => {
     font-weight: 800;
     line-height: 35px;
     margin-top: 13px;
+}
+.live-icon {
+    width: 23.8px;
+    height: 22.2px;
 }
 </style>
