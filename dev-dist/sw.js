353fb2dc3dc13e9a58035a4e6ff2964647891166
Andrew Filkin <filkin.andrew.s@gmail.com>
2025-07-11 08:49:13 +0300

    Vue project moved to root

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-706c6701'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d1er43eeug8"
+    "revision": "0.qdll2slh3v4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
