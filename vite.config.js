58de3ab037bbde18f07878ca1d7700555ba3fc02
RustikDev <rustiknevazno@gmail.com>
2025-08-12 21:24:03 +0300

    Добавил соотсветствующие иконки для манифеста

--- vite.config.js
+++ vite.config.js
@@ -33,12 +33,12 @@ export default defineConfig({
                 theme_color: '#000000',
                 icons: [
                     {
-                        src: '/icon.png',
+                        src: '/icon_192.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: '/icon.png',
+                        src: '/icon_512.png',
                         sizes: '512x512',
                         type: 'image/png',
                     },
