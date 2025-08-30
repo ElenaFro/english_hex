235e96b24fd07b81827dc2ac1559eb0d289b169f
RustikDev <rustiknevazno@gmail.com>
2025-08-30 23:37:30 +0300

    правка видео для айфона

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-706c6701'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cp7l379amlo"
+    "revision": "0.ghpkv4ctdg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Learning/VideoPage.vue
+++ src/components/Learning/VideoPage.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="video-container">
+    <div class="video-container" @click="enableSound">
         <video
             ref="videoRef"
             class="video-container__lerning-video"
             :src="videoUrl"
             autoplay
+            muted
+            playsinline
         ></video>
     </div>
 </template>
@@ -32,24 +34,43 @@ const pauseVideo = () => {
     }
 };
 
+const enableSound = () => {
+    if (videoRef.value) {
+        videoRef.value.muted = false;
+        videoRef.value.play().catch((err) => console.warn('Play failed', err));
+    }
+};
+
+const onVideoError = (event) => {
+    alert('Video error:', event);
+};
+
+const onCanPlay = () => {
+    alert('Video can play');
+
+    videoRef.value.play().catch((error) => {
+        console.error('Play error:', error);
+    });
+};
+
 defineExpose({ replayVideo, pauseVideo });
 </script>
 
 <style scoped>
 .video-container {
-  width: 100%;
-  height: 100%;
-  border-radius: 20px;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .video-container__lerning-video {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 20px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 20px;
 }
 </style>
