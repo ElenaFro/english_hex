64d55340578c0cb13a6d176b16f89a330b33698c
Rustem <gallininadia@gmail.com>
2025-09-07 17:37:14 +0300

    фикс пуш

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-706c6701'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.60n6fn53ce"
+    "revision": "0.mvn2bikhlpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
