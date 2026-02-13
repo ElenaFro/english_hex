d3b6ab273869a4949f4f87e2bc19b728792ca06b
Rustem <topuzrustem73@gmail.com>
2026-02-13 10:28:39 +0300

    Добавил сервисную страницу

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,19 @@ const isAuthenticated = () => {
     return !!localStorage.getItem('access_token');
 };
 
+const MAINTENANCE_MODE = true;
+const MAINTENANCE_ROUTE = '/service';
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
 
 router.beforeEach((to, from, next) => {
+    if (MAINTENANCE_MODE && to.path !== MAINTENANCE_ROUTE) {
+        return next(MAINTENANCE_ROUTE);
+    }
+
     const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
     const isAuth = isAuthenticated();
 
