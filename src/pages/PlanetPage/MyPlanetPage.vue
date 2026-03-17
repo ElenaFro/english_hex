dd76ed4468ff43ec9cb341803e63b789fed78e05
Rustem <topuzrustem73@gmail.com>
2026-03-17 23:48:59 +0300

    добавил установку пройденного хинта панеты

--- src/pages/PlanetPage/MyPlanetPage.vue
+++ src/pages/PlanetPage/MyPlanetPage.vue
@@ -81,6 +81,7 @@ const overlayClass = computed(() => ({ 'index-up': isShowPlanetHint.value }));
 const closePlanetHint = () => {
     isShowPlanetHint.value = false;
     userStore.switchStarOverview(false);
+    localStorage.setItem('planet_hint_shown', 'true');
 };
 
 onMounted(async () => {
