7cde1f07f888400c35b8489efc627f333c54f8af
Rustem <topuzrustem73@gmail.com>
2026-03-17 11:57:07 +0300

    Убрал сервисную страницу

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, START_LOCATION } from 'vue-router';
 import routes from './routes';
 
 const isAuthenticated = () => {
     return !!localStorage.getItem('access_token');
 };
 
-const MAINTENANCE_MODE = true;
+const MAINTENANCE_MODE = false;
 const MAINTENANCE_ROUTE = '/service';
 
 const router = createRouter({
@@ -14,8 +14,11 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    if (MAINTENANCE_MODE && to.path !== MAINTENANCE_ROUTE) {
-        return next(MAINTENANCE_ROUTE);
+    if (MAINTENANCE_MODE) {
+        // Show service page only on the initial app load.
+        if (from === START_LOCATION && to.path !== MAINTENANCE_ROUTE) {
+            return next(MAINTENANCE_ROUTE);
+        }
     }
 
     const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
